08719e7b517fb9f67056f25dac44997bfd07b170
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-22 16:30:57 +0000

    added experimental download button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector(".wrapper"),
 	qrInput = wrapper.querySelector(".form input"),
 	generateBtn = wrapper.querySelector(".form button"),
+	downloadBtn = wrapper.querySelector(".download-btn"),
+	downloadLink = wrapper.querySelector(".download-link"),
 	qrImg = wrapper.querySelector(".qr-code img");
 
 generateBtn.addEventListener("click", () => {
@@ -10,12 +12,21 @@ generateBtn.addEventListener("click", () => {
 	qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
 	qrImg.addEventListener("load", () => {
 		wrapper.classList.add("active");
+		downloadBtn.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
 	});
 });
 
-qrInput.addEventListener("keyup", (e) => {
+qrInput.addEventListener("keyup", () => {
     if (!qrInput.value) {
         wrapper.classList.remove("active");
+		downloadBtn.classList.remove("active");
     }
+})
+
+downloadBtn.addEventListener("click", () => {
+	let imgSrc = qrImg.getAttribute("src");
+	downloadLink.download = "QR Code";
+	downloadLink.href = imgSrc;
+	downloadLink.click();
 })
