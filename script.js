682c01ff2990ce27a28a441ff627fc5418315018
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-08-22 17:55:19 +0000

    enter event added to generate button

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const wrapper = document.querySelector(".wrapper"),
 	downloadLink = wrapper.querySelector(".download-link"),
 	qrImg = wrapper.querySelector(".qr-code img");
 
-generateBtn.addEventListener("click", () => {
-	let qrValue = qrInput.value;
+function generateQRCode() {
+    let qrValue = qrInput.value;
 	if (!qrValue) return;
     generateBtn.innerText = "Generating QR...";
 	qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
@@ -15,7 +15,11 @@ generateBtn.addEventListener("click", () => {
 		downloadBtn.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
 	});
-});
+}
+
+generateBtn.addEventListener("click", generateQRCode);
+
+generateBtn.addEventListener("submit", generateQRCode);
 
 qrInput.addEventListener("keyup", () => {
     if (!qrInput.value) {
@@ -25,23 +29,15 @@ qrInput.addEventListener("keyup", () => {
 })
 
 downloadLink.addEventListener("click", async (event) => {
-    event.preventDefault(); // Prevent the default link behavior
-
+    event.preventDefault();
     try {
-        // Fetch the QR code image using the provided URL
         const response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrInput.value}`);
         const blob = await response.blob();
         const blobUrl = URL.createObjectURL(blob);
-
-        // Create a temporary link element for downloading
         const downloadTempLink = document.createElement("a");
         downloadTempLink.href = blobUrl;
         downloadTempLink.download = "QR_Code.png";
-
-        // Programmatically click the temporary link to initiate download
         downloadTempLink.click();
-
-        // Clean up by revoking the Blob URL
         URL.revokeObjectURL(blobUrl);
     } catch (error) {
         console.error("Error downloading QR code:", error);
